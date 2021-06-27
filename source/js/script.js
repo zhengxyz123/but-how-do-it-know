093d7ed5bc8b380442ab6effa57be83688f0c1e0
jason-bowen-zheng <18917572736@wo.cn>
2021-06-27 18:04:32 +0800

    2021-06-27

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,9 @@
-var split = false;
 r = new marked.Renderer();
 r.image = (href, title, text) => {
 	if (text.indexOf(":left:") == 0) {
-		return `<img alt="${text.slice(6)}" src="${href}">`;	
+		return `<img alt="${text.slice(6)}" src="${href}">`;
+	} else if (text.indexOf(":right:") == 0) {
+		return `<div align="right"><img alt="${text.slice(7)}" src="${href}"></div>`;
 	} else {
 		return `<div align="center"><img alt="${text}" src="${href}"></div>`;
 	}
@@ -19,8 +20,8 @@ function render(f, e) {
 			$(e).html(marked(data));
 			document.title = $(`${e} h1`).first().text();
 			$(e).find("a").each((i, j) => {
-				if ($(j).attr("href").indexOf("//") == -1) {
-					$(j).attr("href", `javascript:render("${$(j).attr("href")}", "${e}")`)
+				if (($(j).attr("href").indexOf("//") == -1) && ($(j).attr("href").indexOf(".md") == $(j).attr("href").length - 3)) {
+					$(j).attr("href", `javascript:render("${$(j).attr("href")}", "${e}")`);
 				}
 			});
 			Cookies.set("page", f, {"expires": 7});
