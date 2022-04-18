2421e3f266fec70fe75e38feb8ea5e75179578c4
jason-bowen-zheng <13239073174@wo.cn>
2022-04-18 08:16:40 +0800

    2022-04-18

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,8 @@
-r = new marked.Renderer();
-r.image = (href, title, text) => {
-	if (text.indexOf(":left:") == 0) {
-		return `<img alt="${text.slice(6)}" src="${href}">`;
-	} else if (text.indexOf(":right:") == 0) {
-		return `<div align="right"><img alt="${text.slice(7)}" src="${href}"></div>`;
-	} else {
-		return `<div align="center"><img alt="${text}" src="${href}"></div>`;
-	}
-}
-marked.setOptions({renderer: r});
-
 function render(f) {
 	$.ajax({
 		url: `books/${f}`,
 		success: (data) => {
-			$("#text").html(marked(data));
+			$("#text").html(marked.parse(data));
 			$(document).title = $("#text h1").first().text();
 			$("#text").find("a").each((i, j) => {
 				if (($(j).attr("href").indexOf("//") == -1) &&
@@ -31,4 +19,3 @@ function render(f) {
 		}
 	});
 }
-
